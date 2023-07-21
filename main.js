90ad448befe8857ba0c736624f9526ec8a05afc3
shorya <94457442+shoryavardhankhyalia@users.noreply.github.com>
2023-07-21 19:06:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+function chapter1()
+{
+    window.location = "chapter1.html";
+}
+function chapter2()
+{
+    window.location = "chapter2.html";
+}
+function chapter3()
+{
+    window.location = "chapter3.html";
+}
+function chapter4()
+{
+    window.location = "chapter4.html";
+}
+function chapter5()
+{
+    window.location = "chapter5.html";
+}
+function chapter6()
+{
+    window.location = "chapter6.html";
+}
+function chapter7()
+{
+    window.location = "chapter7.html";
+}
+function chapter8()
+{
+    window.location = "chapter8.html";
+}
+function chapter9()
+{
+    window.location = "chapter9.html";
+}
+function chapter10()
+{
+    window.location = "chapter10.html";
+}
+function chapter11()
+{
+    window.location = "chapter11.html";
+}
+function chapter12()
+{
+    window.location = "chapter12.html";
+}
+function chapter13()
+{
+    window.location = "chapter13.html";
+}
+function back1()
+{
+    window.location = "app.html";
+}
+
+
